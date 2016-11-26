c52aa607c47ef26d59c2fded5b16ce107f25618f
Code Ass <aycabta@gmail.com>
2016-11-27 04:44:43 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Twitter Gallery Navigation Shrink
+=================================
+
+[This Greasemonkey script on OpenUserJS.org](https://openuserjs.org/scripts/aycabta/Twitter_Gallery_Navigation_Shrink)
+
---
+++ twitter-gallery-nav-shrink.user.js
@@ -0,0 +1,22 @@
+﻿// ==UserScript==
+// @name       Twitter Gallery Nav Shrink
+// @namespace  http://aycabta.github.io/
+// @version    0.0.1
+// @description  Twitter Gallery Nav Shrink
+// @include    /^https?:\/\/twitter.com/
+// @copyright  2016+, Code Ass
+// ==/UserScript==
+
+(function() {
+    var sheet;
+    var style = document.createElement('style');
+    style.setAttribute('id', 'gallery-nav-shrink');
+    style.type = "text/css";
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(style);
+    sheet = style.sheet;
+    sheet.insertRule('.GalleryNav--next {' +
+                     '    width: 33% !important;' +
+                     '}', sheet.cssRules.length);
+})();
+
